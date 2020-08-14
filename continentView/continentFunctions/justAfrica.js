24d948f8770d3384d1d8cc573c23b205993f9aeb
Sebastian Hancock <sebastian.hancock@tum.de>
2020-08-14 12:36:04 +0200

    update info on plantview

--- continentView/continentFunctions/justAfrica.js
+++ continentView/continentFunctions/justAfrica.js
@@ -13,9 +13,9 @@ function justAfrica() {
     }
 
     var files = [
-        d3.json('../continentView/continent-geojsons/africa/africa2.geojson'),
+        d3.json('../continentView/continent-geojsons/africa/africa3.geojson'),
         d3.csv('../continentView/continent-geojsons/africa/africaislands1.csv'),
-        d3.csv('../continentView/continent-geojsons/africa/africaPointsJoined1.csv')
+        d3.csv('../continentView/continent-geojsons/africa/africaPointsW.csv')
 
     ]
 
@@ -61,7 +61,7 @@ function justAfrica() {
             .attr('class', 'continent')
             .attr("d", geoPath)
             .attr('opacity', function(d) {
-                if (d.properties.completeness === 1) {
+                if (d.properties.completene === 1) {
                     return '0.5'
                 } else {
                     return '1'
@@ -70,31 +70,31 @@ function justAfrica() {
             .attr('fill', function(d) {
 
                 return '#e6dccc'
-              /*  if (d.properties.completeness === 1) {
+                /*  if (d.properties.completeness === 1) {
 
-                    const texture3 = textures.lines()
-                        .thicker()
-                        .stroke('black')
-                        //.thinner()
-                        .strokeWidth(0.6)
-                        .background('#dacbb2');
-                    svg.call(texture3)
+                      const texture3 = textures.lines()
+                          .thicker()
+                          .stroke('black')
+                          //.thinner()
+                          .strokeWidth(0.6)
+                          .background('#dacbb2');
+                      svg.call(texture3)
 
-                    return texture3.url()
-                } else if (d.properties.completeness === 2) {
+                      return texture3.url()
+                  } else if (d.properties.completeness === 2) {
 
-                    const texture3 = textures.lines()
-                        //.thicker()
-                        .stroke('black')
-                        //.thinner()
-                        .strokeWidth(0.6)
-                        .background('#dacbb2');
-                    svg.call(texture3)
+                      const texture3 = textures.lines()
+                          //.thicker()
+                          .stroke('black')
+                          //.thinner()
+                          .strokeWidth(0.6)
+                          .background('#dacbb2');
+                      svg.call(texture3)
 
-                    return texture3.url()
-                } else {
-                    return '#dacbb2'
-                } */
+                      return texture3.url()
+                  } else {
+                      return '#dacbb2'
+                  } */
             })
             .attr('stroke', '#ababab')
             .attr('stroke-width', '0.3')
@@ -103,7 +103,7 @@ function justAfrica() {
                     //.duration(200)
                     .style("opacity", .9);
                 tooltip.html("<b>Region Name: </b>" + d.properties.name + "<br>" +
-                        "<b>Country: </b>" + d.properties.completeness)
+                        "<b>Country: </b>" + d.properties.completene)
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY - 28) + "px");
                 d3.select(this).style('stroke', 'yellow')
@@ -140,73 +140,74 @@ function justAfrica() {
 
                 return '#e6dccc'
 
-               /* if (d.completeness === '1') {
+            
+        /* if (d.completeness === '1') {
 
-                    const texture3 = textures.lines()
-                        .thicker()
-                        .stroke('black')
-                        //.thinner()
-                        .strokeWidth(0.8)
-                        .background('#dacbb2');
-                    svg.call(texture3)
+             const texture3 = textures.lines()
+                 .thicker()
+                 .stroke('black')
+                 //.thinner()
+                 .strokeWidth(0.8)
+                 .background('#dacbb2');
+             svg.call(texture3)
 
-                    return texture3.url()
+             return texture3.url()
 
-                } else if (d.completeness === '2') {
+         } else if (d.completeness === '2') {
 
-                    const texture3 = textures.lines()
-                        //.thicker()
-                        .stroke('black')
-                        //.thinner()
-                        .strokeWidth(0.8)
-                        .background('#dacbb2');
-                    svg.call(texture3)
+             const texture3 = textures.lines()
+                 //.thicker()
+                 .stroke('black')
+                 //.thinner()
+                 .strokeWidth(0.8)
+                 .background('#dacbb2');
+             svg.call(texture3)
 
-                    return texture3.url()
+             return texture3.url()
 
-                    
-                } else {
-                    return '#dacbb2'
-                } */
-
-
-            })
-            .on("mouseover", function(d) {
-                tooltip.transition()
-                    //.duration(200)
-                    .style("opacity", .9);
-                tooltip.html("<b>Name: </b>" + d.name + "<br>" + d.completeness)
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
-            })
-            .on("mouseout", function(d) {
-                tooltip.transition()
-                    //.duration(500)
-                    .style("opacity", 0);
-            })
-            .on('click', function(d) {
-                document.getElementById("infoPanel").style.visibility = 'visible'
-            })  
+             
+         } else {
+             return '#dacbb2'
+         } */
 
 
-        g.append('path')
-            .datum(graticule)
-            .attr("class", "graticule1")
-            .attr("d", geoPath)
-            .style('fill-opacity', 0)
-            .style('stroke', 'black')
-            .style('stroke-width', 0.2)
-            .style('stroke-opacity', 0.5)
+    })
+.on("mouseover", function(d) {
+        tooltip.transition()
+            //.duration(200)
+            .style("opacity", .9);
+        tooltip.html("<b>Name: </b>" + d.name + "<br>" + d.completene)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+        tooltip.transition()
+            //.duration(500)
+            .style("opacity", 0);
+    })
+    .on('click', function(d) {
+        document.getElementById("infoPanel").style.visibility = 'visible'
+    })
 
-        /* .on('click', function (d) {
-             _.find(listData, function (o) {
-                 if (o.region_id == d.properties.region_id) {
-                     console.log(o)
-                 }
-             })
-         }) */
+
+g.append('path')
+    .datum(graticule)
+    .attr("class", "graticule1")
+    .attr("d", geoPath)
+    .style('fill-opacity', 0)
+    .style('stroke', 'black')
+    .style('stroke-width', 0.2)
+    .style('stroke-opacity', 0.5)
+
+/* .on('click', function (d) {
+     _.find(listData, function (o) {
+         if (o.region_id == d.properties.region_id) {
+             console.log(o)
+         }
+     })
+ }) */
 
 
-    }
+}
 
 }
